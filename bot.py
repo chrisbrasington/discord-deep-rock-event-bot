30c46d1aea88d3e522cb39023e22130509f658ae
Christopher Brasington <1186896+chrisbrasington@users.noreply.github.com>
2022-12-14 22:45:18 -0700

    exists

--- bot.py
+++ bot.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python3
 import discord
 from datetime import datetime, timedelta
+from discord.ext import commands
 
 # create a discord client
 client = discord.Client(intents=discord.Intents.all())
+# client = commands.Bot(
+#     command_prefix="/", 
+#     case_insensitive=True, 
+#     intents=discord.Intents.all())
+
 
 event_names = [
     'Deep Rockin\' Wednesdays',
@@ -26,8 +33,10 @@ event_names = [
 #     await event.send(f"Event '{name}' has been created at {time} with image {image}")
 
 # handle the ready event
-# @client.event
-# async def on_ready():
+@client.event
+async def on_ready():
+    global guild_id
+
     # # get the current guild
     # guild = discord.utils.get(client.guilds, name="My Guild")
     # # check if the event exists
@@ -40,7 +49,21 @@ event_names = [
     # if not event_exists:
     #     await create_event(guild, event_name, event_time, event_image)
 
-async dev has_event():
+    guild = client.get_guild(guild_id)
+    print(f'{guild.name} events')
+
+    found = False
+
+    for event in guild.scheduled_events:
+        print(event.name)
+        if event.name in event_names:
+            found = True
+    
+    if found:
+        print('Exists')
+    else:
+        print('Not found')
+
 
 
 @client.event
@@ -56,9 +79,15 @@ async def on_message(message):
     if respond:
         await message.channel.send(f"Rock and Stone! {message.author.mention}")
 
+    await has_event()
+
 # read the token from the file
 with open("bot_token.txt", "r") as f:
     token = f.read()
+
+with open("guild_id.txt", "r") as f:
+    guild_id = int(f.read())
+
 
 # run the bot
 client.run(token)
